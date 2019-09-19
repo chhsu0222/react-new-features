7cf2ffa0758ecc6902325117783141aef02805e1
ch <ch@mail.startek-eng.com>
2019-09-19 10:52:16 +0800

    Introduce the useEffect hook
    sec. 19, video 193
    
    Ref: https://reactjs.org/docs/hooks-effect.html
    
    useEffect allows us to do something in functional components. This is
    kind of a replacement for lifecycle methods in our class based
    components (you can think of useEffect Hook as componentDidMount,
    componentDidUpdate, and componentWillUnmount combined.).
    
    In React class components, the render method itself shouldn’t cause side
    effects. It would be too early — we typically want to perform our
    effects after React has updated the DOM.
    This is why in React classes, we put side effects into componentDidMount
    and componentDidUpdate.
    
    useEffect is something we call and we pass to a function. The function
    is going to run once right away with our default state values
    (componentDidMount) and after changes to our component state or props
    (componentDidUpdate).
    For example, we can use it to synchronize the count state with the
    document title.
    
    For <NoteApp>, we can read notes data from localStorage (default to
    empty array) and call useEffect to update localStorage when note array
    changes.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
 const NoteApp = () => {
-    const [notes, setNotes] = useState([])
+    const notesData = JSON.parse(localStorage.getItem('notes'))
+    const [notes, setNotes] = useState(notesData || [])
     const [title, setTitle] = useState('')
     const [body, setBody] = useState('')
 
@@ -20,6 +21,10 @@ const NoteApp = () => {
     const removeNote = (title) => {
         setNotes(notes.filter((note) => note.title !== title))
     }
+
+    useEffect(() => {
+        localStorage.setItem('notes', JSON.stringify(notes))
+    })
 
     return (
         <div>
@@ -46,27 +51,32 @@ const NoteApp = () => {
         </div>
     )
 }
-// const App = (props) => {
-//     const [count, setCount] = useState(props.count)
-//     const [text, setText] = useState('')
 
-//     const increment = () => {
-//         setCount(count + 1)
-//     }
-//     const decrement = () => {
-//         setCount(count + 1)
-//     }
+const App = (props) => {
+    const [count, setCount] = useState(props.count)
+    const [text, setText] = useState('')
 
-//     return (
-//         <div>
-//             <p>The current {text || 'count'} is {count}</p>
-//             <button onClick={increment}>+1</button>
-//             <button onClick={decrement}>-1</button>
-//             <button onClick={() => setCount(props.count)}>reset</button>
-//             <input value={text} onChange={(e) => setText(e.target.value)}/>
-//         </div>
-//     )
-// }
+    useEffect(() => {
+        document.title = count
+    })
+
+    const increment = () => {
+        setCount(count + 1)
+    }
+    const decrement = () => {
+        setCount(count + 1)
+    }
+
+    return (
+        <div>
+            <p>The current {text || 'count'} is {count}</p>
+            <button onClick={increment}>+1</button>
+            <button onClick={decrement}>-1</button>
+            <button onClick={() => setCount(props.count)}>reset</button>
+            <input value={text} onChange={(e) => setText(e.target.value)}/>
+        </div>
+    )
+}
 
 ReactDOM.render(<NoteApp/>, document.getElementById('root'));
 
